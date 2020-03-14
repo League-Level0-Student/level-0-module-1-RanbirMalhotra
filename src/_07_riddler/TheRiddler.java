7efea87385adebab959526029a803f4b866a27ed
RanbirMalhotra <60302345+RanbirMalhotra@users.noreply.github.com>
2020-03-14 15:49:16 -0700

    Update TheRiddler.java
    Level 0 Day 1

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -1,4 +1,7 @@
 package _07_riddler;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
@@ -9,13 +12,19 @@ public class TheRiddler {
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String input = JOptionPane.showInputDialog(null, "Who makes it and has no use of it, Who buys but dosen't need it, and who gets it but can neither feel nor see it. What is it? ");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if ("Coffin") 
+	String input = JOptionPane.showInputDialog(null, "CORRECT! How do you feel?");
+{
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+else { 
+	String input = JOptionPane.showInputDialog
+	
+}
 		// 6. Add some more riddles
 
 		// 2. Make a pop up to show the score.
