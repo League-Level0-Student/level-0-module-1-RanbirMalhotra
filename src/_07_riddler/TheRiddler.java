909c7fcf88cb15f1c758389dd6c2051d531172ee
RanbirMalhotra <60302345+RanbirMalhotra@users.noreply.github.com>
2020-03-28 15:49:53 -0700

    Update TheRiddler.java

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -16,8 +16,10 @@ int score = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
 String input = JOptionPane.showInputDialog(null, "Who makes it and has no use of it, Who buys but dosen't need it, and who gets it but can neither feel nor see it. What is it? ");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-if ("Coffin") 
-	String input = JOptionPane.showInputDialog(null, "CORRECT! How do you feel?");
+Object String;
+if ("Coffin" != null)
+	
+String input = JOptionPane.showInputDialog(null, "CORRECT! How do you feel?");
 {
 }
 		// 5. Otherwise, say "wrong" and tell them the answer
